f83a30816ce07726000a4c04a33a06c6fe9c9367
Kyriakos <kchristodoulidis@isc.tuc.gr>
2021-05-28 07:44:04 +0300

    Updates

--- BoardGame/Assets/Scripts/DiceScripts/DiceScript.cs
+++ BoardGame/Assets/Scripts/DiceScripts/DiceScript.cs
@@ -22,10 +22,10 @@ public class DiceScript : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			DiceNumberTextScript.diceNumber = 0;
-			float dirX = Random.Range(0, 500);
-			float dirY = Random.Range(0, 500);
-			float dirZ = Random.Range(0, 500);
-			transform.position = new Vector3(0, 2, 0);
+			float dirX = Random.Range(10, 500);
+			float dirY = Random.Range(10, 500);
+			float dirZ = Random.Range(10, 500);
+			transform.position = new Vector3(477, 12, 73);
 			transform.rotation = Quaternion.identity;
 			rb.AddForce(transform.up * 500);
 			rb.AddTorque(dirX, dirY, dirZ);
